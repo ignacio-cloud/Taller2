bea44c5e5afd0cea29b6c1c3aab15d1a4b31a5a7
Chris <bryanignacio99@gmail.com>
2020-10-17 00:59:21 -0600

    termino_funcion

--- Funcion.py
+++ Funcion.py
@@ -1,5 +1,5 @@
 import pytz
-import timezone
+import datetime
 
 class Funcion(self):
     def __init__(self, nombre, horario):
@@ -10,3 +10,19 @@ class Funcion(self):
 def disponible(self):
     timezone = pytz.timezone('America/Guatemala')
     fecha_completa = datetime.datetime.now(tz = timezone)
+    hora = fecha_completa.strftime('%H')
+    minutos = fecha_completa.strftime('%M')
+    hora_actual = int(hora)
+    min_actual = int(minutos)
+    #hacemos un split
+    tiempo_funcion = self.horario.split(':')
+    #vamos a recibir 24:00 por lo que en arr[0] = 24 y arr[1] = 00
+    hora_funcion = int(tiempo_funcion[0])
+    min_funcion = int(tiempo_funcion[1])
+    #validamos
+    if hora_actual > hora_funcion:
+        return False
+    elif hora_actual == hora_funcion:
+        if min_actual > min_funcion:
+            return False
+    return True
