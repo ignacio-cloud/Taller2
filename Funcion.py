2b776148a1f1934149b4c2541583a50f9a69bde1
Chris <bryanignacio99@gmail.com>
2020-10-17 00:40:35 -0600

    se agrego funcion

---
+++ Funcion.py
@@ -0,0 +1,12 @@
+import pytz
+import timezone
+
+class Funcion(self):
+    def __init__(self, nombre, horario):
+        self.nombre = nombre
+        self.horario = horario
+        self.asistentes = []
+
+def disponible(self):
+    timezone = pytz.timezone('America/Guatemala')
+    fecha_completa = datetime.datetime.now(tz = timezone)
